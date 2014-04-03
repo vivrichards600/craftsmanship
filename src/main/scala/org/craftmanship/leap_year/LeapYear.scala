ec606686ecb1213f885e07addaf9d04bb67c031d
richarv <vivrichards600@hotmail.com>
2014-04-03 14:31:02 +0100

    refactored leap year exercise

--- src/main/scala/org/craftmanship/leap_year/LeapYear.scala
+++ src/main/scala/org/craftmanship/leap_year/LeapYear.scala
@@ -1,14 +1,10 @@
 package org.craftmanship.leap_year
 
-
 class Year(year: Int) {
-  def isLeapYear: Boolean = {
-    if (isNotDivisibleBy4 || isNotDivisibleBy100 && isDivisibleBy400) return false
-    true
-  }
-
-  private def isNotDivisibleBy4 = year % 4 != 0
-  private def isNotDivisibleBy100 = year % 100 == 0
-  private def isDivisibleBy400 = year % 400 != 0
+  def isLeapYear: Boolean = divisibleBy4 && !(divisibleBy100 && notDivisibleBy400)
+  
+  private def divisibleBy4 = year % 4 == 0
+  private def divisibleBy100 = year % 100 == 0
+  private def notDivisibleBy400 = year % 400 != 0
 }
 
