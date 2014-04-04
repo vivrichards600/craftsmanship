6e7e32b8d8687bc58e3e171b39928178fcac888e
richarv <vivrichards600@hotmail.com>
2014-04-04 13:17:19 +0100

    tests added, code refactored

--- src/main/scala/org/craftmanship/user_validation/UserService.scala
+++ src/main/scala/org/craftmanship/user_validation/UserService.scala
@@ -1,16 +1,9 @@
 package org.craftmanship.user_validation
 
-class UserService(userRepository: UserRepository) {
+class UserService(userRepository: UserRepository, userValidation: UserValidation) {
 
   def save(user: User) {
-
-    if (user.firstname.isEmpty) throw new IllegalArgumentException
-
-    if (user.surname.isEmpty) throw new IllegalArgumentException
-
-    val EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$".r
-    if (!EMAIL_PATTERN.pattern.matcher(user.emailaddress).matches) throw new IllegalArgumentException
-
+    userValidation.validate(user)
     userRepository.save(user)
   }
 }
--- src/test/scala/org/craftmanship/user_validation/UserServiceSpec.scala
+++ src/test/scala/org/craftmanship/user_validation/UserServiceSpec.scala
@@ -2,38 +2,59 @@ package org.craftmanship.user_validation
 
 import org.craftmanship.spec.UnitSpec
 import java.lang.IllegalArgumentException
-import org.mockito.Mockito.verify
+import org.mockito.Mockito._
+import org.mockito.BDDMockito._
+import scala.util.Try
 
 class UserServiceSpec extends UnitSpec {
   "User service" should {
-    "not save a user when first name is not valid" in {
-      val invalid_user = User(INVALID_FIRSTNAME, SURNAME, EMAIL_ADDRESS)
+//    "not save a user when first name is not valid" in {
+//      val invalid_user = User(INVALID_FIRSTNAME, SURNAME, EMAIL_ADDRESS)
+//
+//      an [IllegalArgumentException] should be thrownBy userService.save(invalid_user)
+//    }
+//
+//    "not save a user when surname is not valid" in {
+//      val invalid_user = User(FIRSTNAME, INVALID_SURNAME, EMAIL_ADDRESS)
+//
+//      an [IllegalArgumentException] should be thrownBy userService.save(invalid_user)
+//    }
+//
+//    "not save a user when email address is not valid" in {
+//      val invalid_user = User(FIRSTNAME, SURNAME, INVALID_EMAIL_ADDRESS)
+//
+//      an [IllegalArgumentException] should be thrownBy userService.save(invalid_user)
+//    }
+//
+//    "save a valid user" in {
+//      val user = User(FIRSTNAME, SURNAME, EMAIL_ADDRESS)
+//
+//      userService.save(user)
+//
+//      verify(userRepository) save(user)
+//    }
 
-      an [IllegalArgumentException] should be thrownBy userService.save(invalid_user)
-    }
+    "not save a user when user is invalid" in {
+      given(userValidation.validate(INVALID_USER)) willThrow(new IllegalArgumentException)
 
-    "not save a user when surname is not valid" in {
-      val invalid_user = User(FIRSTNAME, INVALID_SURNAME, EMAIL_ADDRESS)
+      intercept[IllegalArgumentException] {
+        userService save INVALID_USER
+      }
 
-      an [IllegalArgumentException] should be thrownBy userService.save(invalid_user)
-    }
-
-    "not save a user when email address is not valid" in {
-      val invalid_user = User(FIRSTNAME, SURNAME, INVALID_EMAIL_ADDRESS)
-
-      an [IllegalArgumentException] should be thrownBy userService.save(invalid_user)
+      verifyZeroInteractions(userRepository)
     }
 
     "save a valid user" in {
-      val user = User(FIRSTNAME, SURNAME, EMAIL_ADDRESS)
+        userService save USER
 
-      userService.save(user)
-
-      verify(userRepository) save(user)
+        verify(userValidation) validate USER
+        verify(userRepository) save USER
     }
+
   }
   val userRepository = mock[UserRepository]
-  val userService = new UserService(userRepository)
+  val userValidation = mock[UserValidation]
+  val userService = new UserService(userRepository, userValidation)
 
   val FIRSTNAME = "VIV"
   val SURNAME = "SMITH"
@@ -41,5 +62,7 @@ class UserServiceSpec extends UnitSpec {
   val INVALID_SURNAME = ""
   val EMAIL_ADDRESS = "valid@valid.co.uk"
   val INVALID_EMAIL_ADDRESS = "invalid"
-  
+  val INVALID_USER = User("", "", "")
+  val USER = User("Alice", "Smith", "a@a.com")
+
 }
