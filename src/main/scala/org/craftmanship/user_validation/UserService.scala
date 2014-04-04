fb4e25e14d1c7e8596b959164d887d132cdf2833
richarv <vivrichards600@hotmail.com>
2014-04-04 12:21:17 +0100

    code cleaned up so conditions are not nested for validation

--- src/main/scala/org/craftmanship/user_validation/UserService.scala
+++ src/main/scala/org/craftmanship/user_validation/UserService.scala
@@ -3,14 +3,14 @@ package org.craftmanship.user_validation
 class UserService(userRepository: UserRepository) {
 
   def save(user: User) {
-    val EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$".r
 
-    EMAIL_PATTERN.pattern.matcher(user.emailaddress).matches match {
-      case true => {
-        if ((user.firstname.length < 1) || (user.surname.length < 1)) throw new IllegalArgumentException
-        else userRepository.save(user)
-      }
-      case false => throw new IllegalArgumentException
-    }
+    if (user.firstname.isEmpty) throw new IllegalArgumentException
+
+    if (user.surname.isEmpty) throw new IllegalArgumentException
+
+    val EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$".r
+    if (!EMAIL_PATTERN.pattern.matcher(user.emailaddress).matches) throw new IllegalArgumentException
+
+    userRepository.save(user)
   }
 }
--- src/test/scala/org/craftmanship/user_validation/UserServiceSpec.scala
+++ src/test/scala/org/craftmanship/user_validation/UserServiceSpec.scala
@@ -7,29 +7,29 @@ import org.mockito.Mockito.verify
 class UserServiceSpec extends UnitSpec {
   "User service" should {
     "not save a user when first name is not valid" in {
-      val user = User(INVALID_FIRSTNAME, SURNAME, EMAIL_ADDRESS)
+      val invalid_user = User(INVALID_FIRSTNAME, SURNAME, EMAIL_ADDRESS)
 
-      an [IllegalArgumentException] should be thrownBy userService.save(user)
+      an [IllegalArgumentException] should be thrownBy userService.save(invalid_user)
     }
 
     "not save a user when surname is not valid" in {
-      val user = new User(FIRSTNAME, INVALID_SURNAME, EMAIL_ADDRESS)
+      val invalid_user = User(FIRSTNAME, INVALID_SURNAME, EMAIL_ADDRESS)
 
-      an [IllegalArgumentException] should be thrownBy userService.save(user)
+      an [IllegalArgumentException] should be thrownBy userService.save(invalid_user)
     }
 
     "not save a user when email address is not valid" in {
-      val user = new User(FIRSTNAME, SURNAME, INVALID_EMAIL_ADDRESS)
+      val invalid_user = User(FIRSTNAME, SURNAME, INVALID_EMAIL_ADDRESS)
 
-      an [IllegalArgumentException] should be thrownBy userService.save(user)
+      an [IllegalArgumentException] should be thrownBy userService.save(invalid_user)
     }
 
     "save a valid user" in {
-      val user = new User(FIRSTNAME, SURNAME, EMAIL_ADDRESS)
+      val user = User(FIRSTNAME, SURNAME, EMAIL_ADDRESS)
 
       userService.save(user)
 
-      verify(userRepository).save(user)
+      verify(userRepository) save(user)
     }
   }
   val userRepository = mock[UserRepository]
